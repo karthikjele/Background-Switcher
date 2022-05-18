caeb793f3046ee09d92f38aaff19d72bd34f295e
karthik <karthikj.ele@gmail.com>
2022-05-18 01:20:51 -0700

    Background-Switcher

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function changeBg(color) {
+    document.body.style.backgroundColor=color;
+    let txt=document.getElementsByClassName("text");
+
+    
+    if( color=="#000000")
+    {
+        for(let elm of txt){
+            elm.style.color="#ffffff";
+        }
+    }else{
+        for(let elm of txt){
+            elm.style.color="000000";
+        }
+    }
+    
+};
